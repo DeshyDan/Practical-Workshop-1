ee2882df15483cb652e60997b0135561bed23a43
Deshy Dan <deshy733@gmail.com>
2023-07-28 22:07:30 +0200

    Added july class

---
+++ July.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practical_Workshop_1
+{
+    internal class July
+    {
+        List<Horse> horses = new List<Horse>();
+
+        public void addHorse(Horse horse)
+        {
+            horses.Add(horse);
+        }
+        public void displarRoster()
+        {
+
+        }
+        public Horse favourite()
+        {
+            double maxAverageTime = double.MinValue;
+            Horse horseWithMaxAvgTime = null;
+
+
+            foreach (Horse horse in horses)
+            {
+                if (horse.averageTime() > maxAverageTime)
+                {
+                    maxAverageTime = horse.averageTime();
+                    horseWithMaxAvgTime = horse;
+                }
+            }
+            return horseWithMaxAvgTime;
+        }
+    }
+}
